353b332b1cd9ff2fb5d53ca466b02bf047d1eefe
NickWang <hcfw007@126.com>
2018-05-30 16:47:57 +0800

    render object to dom

--- index.js
+++ index.js
@@ -19,6 +19,33 @@ function h(type, props, ...children) {
   }
 }
 
-function render() {
-  console.log(view())
+function render(root) {
+  root.appendChild(createElement(view(0)))
+}
+
+function createElement(node) {
+  if (typeof(node) === 'string') {
+    return document.createTextNode(node)
+  }
+  let { type, props, children } = node
+  const el = document.createElement(type)
+  setProps(el, props)
+  children.map(value => createElement(value)).forEach(value => {
+    el.appendChild(value)
+  })
+  return el
+}
+
+function setProp(target, name, value) {
+  if (name === 'className') {
+    return target.setAttribute('class', value)
+  }
+
+  target.setAttribute(name, value)
+}
+
+function setProps(target, props) {
+  Object.keys(props).forEach(key => {
+    setProp(target, key, props[key])
+  })
 }
